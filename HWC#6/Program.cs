4883387ce2ac8ac129cae7b653462d2be17c0d71
Alesia <andomyel@yandex.ru>
2022-10-25 17:38:29 +0300

    HWC#6tesk2

--- HWC#6/Program.cs
+++ HWC#6/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // Первый вариант решения
+/*
 int[] Array = GetArray(5);
 Console.WriteLine(String.Join(" ", Array));
 Console.WriteLine($"Вы ввели {numplus(Array)}  чисел больше нуля");
@@ -25,7 +26,7 @@ int[] GetArray (int size){
     return res;
 }
 
-
+*/
 
 
 /*
@@ -49,6 +50,30 @@ for (int i = 0; i < Array.Length; i++) {
 }
 Console.WriteLine($"Вы ввели {count} цифр(у,ы) больше нуля");
 */
+
+
+//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("Введите  целое число b1");
+double b1 = Convert.ToDouble(Console.ReadLine()!);
+
+Console.WriteLine("Введите  целое число k1");
+double k1 = Convert.ToDouble(Console.ReadLine()!);
+
+Console.WriteLine("Введите  целое число b2");
+double b2 = Convert.ToDouble(Console.ReadLine()!);
+
+Console.WriteLine("Введите  целое число k2");
+double k2 = Convert.ToDouble(Console.ReadLine()!);
+if (k1 == k2){
+     Console.WriteLine("Прямые, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2 параллельны и не пересекаются");
+}
+else {
+    double x = (b1 - b2) / (k2 - k1);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Координаты точки пересечения прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2 ({x:f3};{y:f3})");
+}
+
 
 
 
